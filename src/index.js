f90b0ca0112be5c932278024530e390af9cd17c6
Ben Hong <bencodezen@gmail.com>
2017-07-11 11:36:21 -0400

    config: Add initial template files

---
+++ src/index.js
@@ -0,0 +1,23 @@
+var Alexa = require('alexa-sdk');
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+
+    // alexa.appId = 'amzn1.echo-sdk-ams.app.1234';
+
+    ///alexa.dynamoDBTableName = 'YourTableName'; // creates new table for session.attributes
+
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('MyIntent');
+    },
+
+    'MyIntent': function () {
+        this.emit(':tell', 'Hello World from Alexa');
+    }
+};
+
